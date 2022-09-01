714cfe461836f3fd2bb0505f865cdb64dcc7d6e0
taskevich <93867297+taskevich@users.noreply.github.com>
2022-09-01 18:33:13 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,7 @@
 <?
 
-    try {
+    try 
+    {
         $conn = new PDO("mysql:host=localhost;dbname=users", "root", "root");
     } catch (PDOException $e) {
         echo "connection failed: ".$e->message;
